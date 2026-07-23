e82ad9b6610230859bfbcc86f0f868e2de29c467
joao <pato125125@gmail.com>
2026-07-22 21:56:22 -0300

    Style:Colorama feito

--- menu.py
+++ menu.py
@@ -1,7 +1,9 @@
+from colorama import Fore, Style, init   
+init ()
 def menu ():
     while True:
-        print ("=================== MENU ===================\n\n")
-        escolha = float(input("OPÇÕES\n\n1- CADASTRO\n2- LISTA DE CADASTROS\n3- ALTERAR DADOS\n4- EXCLUIR DADOS\n5- SAIR\nDigite sua escolha: "))
+        print (Fore.LIGHTBLUE_EX + "==============================\n Sistema de Cadastro \n==============================\n")
+        escolha = int(input(Fore.LIGHTCYAN_EX + "OPÇÕES\n\n1- CADASTRO\n2- LISTA DE CADASTROS\n3- ALTERAR DADOS\n4- EXCLUIR DADOS\n5- SAIR\nDigite sua escolha: "))
         if escolha == 1:
             cadastro()
         if escolha == 2:
@@ -11,25 +13,25 @@ def menu ():
         if escolha == 4:
             excluir()
         if escolha == 5:
-            print ("FECHANDO PROGRAMA...")
+            print (Fore.BLUE + "\nFECHANDO PROGRAMA...")
             break
 
 def cadastro ():
-    print ("\n==== CADASTRAR ALUNOS ===")
-    nome_errado = input("Digite o nome do aluno: ")
+    print (Fore.LIGHTWHITE_EX + "\n==== CADASTRAR ALUNOS ===")
+    nome_errado = input(Fore.LIGHTWHITE_EX + "Digite o nome do aluno: ")
     nome = nome_errado.strip()
     
     
     #validação da nota
-    nota_str = input("Digite a nota do aluno: ")
+    nota_str = input(Fore.LIGHTWHITE_EX + "Digite a nota do aluno: ")
     #converçaõ de " , " para " . "
     nota_str = nota_str.replace(",", ".")
     nota = float(nota_str)
 
      
     while nota <0 or nota  >10:
-        print ("Digite uma nota valida!\n")
-        nota_str = input("Digite a nota do aluno: ")
+        print (Fore.LIGHTRED_EX + "Digite uma nota valida!\n")
+        nota_str = input(Fore.LIGHTWHITE_EX + "Digite a nota do aluno: ")
         nota_str = nota_str.replace(",", ".")
         nota = float(nota_str)
 
@@ -41,7 +43,7 @@ def cadastro ():
     
     with open ("dados_alunos.txt", "a", encoding= "utf-8") as arquivo:
         arquivo.write(f"{nome};{nota:.1f};{estado}\n")
-    print ("Aluno cadastrado com sucesso!\n")
+    print (Fore.LIGHTGREEN_EX + "Aluno cadastrado com sucesso!\n")
     
 
 
@@ -50,8 +52,14 @@ def mostrar ():
     with open ("dados_alunos.txt", "r", encoding="UTF-8") as arquivo:
         conteudo = arquivo.readlines()
 
-    print (f"{'NOME':<10} {'NOTA':<10} ESTADO")
+    if not conteudo:
+        print(Fore.LIGHTRED_EX + "\nNenhum aluno cadastrado!\n")
+        return
+    
+    print (Fore.LIGHTYELLOW_EX + "\n==== LISTA DE CADASTRADOS ===\n")
+    print (Fore.LIGHTWHITE_EX + f"{'NOME':<10} {'NOTA':<10} ESTADO")
     print ("=" * 35)
+
 
     for linhas in conteudo:
         linhas = linhas.strip()
@@ -60,18 +68,19 @@ def mostrar ():
         nota = float(partes[1])
         estado = partes[2]
 
-        print(f"{nome:<10} {nota:<10} {estado}")
+        print(Fore.LIGHTWHITE_EX + f"{nome:<10} {nota:<10} {estado}")
+    print ("\n")
     
 
 
 
 
 def alterar ():
-    print ("\n=== ALTERAR NOTA ===")
+    print (Fore.LIGHTMAGENTA_EX + "\n=== ALTERAR NOTA ===")
     with open ("dados_alunos.txt", "r", encoding="UTF-8") as arquivo:
         cont_p_alterar = arquivo.readlines() #CRIA UMA LISTA COM OS DADOS LIDOS 
 
-    nome_aluno = input("Digite o nome do aluno que deseja alterar a nota: ")
+    nome_aluno = input(Fore.LIGHTWHITE_EX + "Digite o nome do aluno que deseja alterar a nota: \n")
 
     existe = False #CRIA UMA VARIAVEL BOLEANA 
     cont_alterado = [] #CRIA UMA LISTA VAZIA PARA OS NOVOS DADOS
@@ -90,14 +99,14 @@ def alterar ():
 
      if nome.lower() == nome_aluno.lower():#IDENTIFICA SE O NOME DIGITADO EXISTE NOS CADASTROS
         existe = True#MUDA A VARIAVEL BOLEANA PARA VERDADEIRA 
-        print (f"ALUNO ENCONTRADO\nNOME: {nome} | NOTA ATUAL: {nota:.2} | ESTADO ATUAL: {estado}")
-        nova_nota = input("Digite a nova nota: ")#PEDE UMA NOVA NOTA 
+        print (Fore.LIGHTGREEN_EX + f"ALUNO ENCONTRADO\nNOME: {nome} | NOTA ATUAL: {nota:.2} | ESTADO ATUAL: {estado}")
+        nova_nota = input(Fore.LIGHTWHITE_EX + "Digite a nova nota: ")#PEDE UMA NOVA NOTA 
         nova_nota = nova_nota.replace(",", ".")
         nova_nota = float(nova_nota)
 
         while nova_nota <0 or nova_nota >10:#PEDE UMA NOVA NOTA CASO DIGITEM UMA NOTA MAIOR QUE 10 OU MENOR QUE 0
-            print ("DIGITE UMA NOTA VALIDA!")
-            nova_nota = input("Digite a nova nota: ")#PEDE UMA NOVA NOTA 
+            print (Fore.LIGHTRED_EX + "DIGITE UMA NOTA VALIDA!")
+            nova_nota = input(Fore.LIGHTWHITE_EX + "Digite a nova nota: ")#PEDE UMA NOVA NOTA 
             nova_nota = nova_nota.replace(",", ".")
             nova_nota = float(nova_nota)
 
@@ -108,23 +117,23 @@ def alterar ():
       cont_alterado.append(n)
     
     if existe == True:
-       print ("NOTA ALTERADA COM SUCESSO!\n")
+       print (Fore.LIGHTGREEN_EX + "NOTA ALTERADA COM SUCESSO!\n")
        with open ("dados_alunos.txt", "w", encoding="UTF-8") as arquivo:
           arquivo.writelines(cont_alterado)
     
     else:
-       print ("aluno não encontrado!\n")
+       print (Fore.LIGHTRED_EX + "aluno não encontrado!\n")
     
 
 
 
 def excluir ():
-    print ("\n=== EXCLUIR ALUNO ===")
+    print (Fore.LIGHTRED_EX + "\n=== EXCLUIR ALUNO ===")
     with open ("dados_alunos.txt", "r", encoding="UTF-8") as arquivo:
         conteudo = arquivo.readlines()
 
 
-    nome_excluir = input("DIGITE O NOME DO ALUNO QUE DESEJA EXCLUIR: ")
+    nome_excluir = input(Fore.LIGHTWHITE_EX + "DIGITE O NOME DO ALUNO QUE DESEJA EXCLUIR: \n")
 
     existe_1 = False 
 
@@ -137,26 +146,26 @@ def excluir ():
 
         if nome_excluir.lower() == nome.lower():
             existe_1 = True 
-            print (f"ALUNO ENCONTRADO!\nNOME: {nome} NOTA: {nota} ESTADO: {estado}\n")
+            print (Fore.LIGHTGREEN_EX + f"ALUNO ENCONTRADO!\nNOME: {nome} NOTA: {nota} ESTADO: {estado}\n")
 
             while True:
-                escolha =  input("TEM CERTEZA QUE DESEJA EXCLUIR ESSE ALUNO DO SISTEMA? (S/N)\n").lower()
+                escolha =  input(Fore.LIGHTYELLOW_EX + "TEM CERTEZA QUE DESEJA EXCLUIR ESSE ALUNO DO SISTEMA? (S/N)\n").lower()
                 if escolha == "s":
                     conteudo.remove(n)
                     with open ("dados_alunos.txt", "w", encoding="UTF-8") as arquivo:
                         arquivo.writelines(conteudo)
-                    print ("ALUNO EXCLUIDO COM SUCESSO!\n")
+                    print (Fore.LIGHTGREEN_EX + "ALUNO EXCLUIDO COM SUCESSO!\n")
                     break
                     
                 if escolha == "n":
-                    print ("EXCLUSÃO CANCELADA!\n")
+                    print (Fore.LIGHTMAGENTA_EX + "EXCLUSÃO CANCELADA!\n")
                     break
                 else:
-                    print ("DIGITE APENAS 'S' ou 'N'!\n")
+                    print (Fore.LIGHTRED_EX + "DIGITE APENAS 'S' ou 'N'!\n")
                 
 
     if existe_1 == False:
-        print ("ALUNO NÃO ENCONTRADO!\n")    
+        print (Fore.LIGHTRED_EX + "ALUNO NÃO ENCONTRADO!\n")    
     
 
 
